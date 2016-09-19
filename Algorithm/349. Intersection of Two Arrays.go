b901f71e34afa16745b23ecad583b1c623cb87b8
lwh <lwhile@outlook.com>
2016-09-19 19:47:22 +0800

    Create 349. Intersection of Two Arrays.go

---
+++ Algorithm/349. Intersection of Two Arrays.go
@@ -0,0 +1,20 @@
+func intersection(nums1 []int, nums2 []int) []int {
+       var result = []int{}
+
+    m := make(map[int]bool)
+
+    for _,num := range nums1 {
+        if _, ok := m[num]; !ok {
+            m[num] = false
+        }
+    }
+
+    for _, num := range nums2 {
+        if value,ok:=m[num]; ok && value==false{
+            m[num] = true
+            result = append(result,num)
+        }
+    }
+
+    return result 
+}
